fdf9d3fa90fb03582f722222543565309bc12f52
Allen <allenfan@live.com>
2016-07-14 19:39:49 -0400

    controller.cc
    update

--- code/Controller.cc
+++ code/Controller.cc
@@ -1,4 +1,15 @@
-int main()
-{
+#include "Player.h"
+#include "Game.h"
+#include "Move.h"
+#include <iostream>
+#include <vector>
+#include <utility>
+
+
+
+int main(){
+	Game * curGame = new Game{}; // need edit
+	string input;
+	while (cin >> input)
 
 }
