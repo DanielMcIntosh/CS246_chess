a27b165698e5e1aa715c85db9a2923311adacbba
vusause <mikevu123@gmail.com>
2016-07-14 18:37:36 -0400

    Completed Knight Class

---
+++ code/Knight.cc
@@ -0,0 +1,30 @@
+#include <vector>
+#include <utility>
+#include "Knight.h"
+
+use namespace std;
+
+Knight::Knight(bool): colour{colour} {};
+
+char Knight::getChar() {colour ? return 'k' : return 'K';};
+
+bool Knight::isValid(pair<int, int> co) {
+    if(co.first == 1 || co.first == -1) {
+        if(co.second == 2 || co.second == -2) {
+            return true;
+        } else {
+            return false;
+        }
+    } else(co.first == 2 || co.first == -2) {
+        if(co.second == 1 || co.second == -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+vector<pair<int,int>> Pawn::getMoveReq(pair<int,int> co) {
+    vector<pair<int,int>> req = {};
+    return req;
+}
---
+++ code/Knight.h
@@ -0,0 +1,16 @@
+#IFNDEF _KNIGHT_H_
+#DEFINE _KNIGHT_H_
+#include <vector>
+#include <utility>
+#include "Piece.h"
+
+class Knight: public Piece {
+public:
+    Knight(bool colour);
+    bool isValid(std::pair<int,int> co);
+    //Takes in difference in position for the move returns the required relative empty spaces
+    std::vector<std::pair<int,int>> getMoveReq(std::pair<int,int> co);
+    char getChar();
+};
+
+#endif
