169991a44e06ccfcf7854aa28280b2de4e09940a
vusause <mikevu123@gmail.com>
2016-07-15 01:49:32 -0400

    Plan of Attack

--- code/Knight.h
+++ code/Knight.h
@@ -7,9 +7,9 @@
 class Knight: public Piece {
 public:
     Knight(bool colour);
-    bool isValid(std::pair<int,int> co);
+    bool isValid(std::pair<int,int> co) override;
     //Takes in difference in position for the move returns the required relative empty spaces
-    std::vector<std::pair<int,int>> getMoveReq(std::pair<int,int> co);
+    std::vector<std::pair<int,int>> getMoveReq(std::pair<int,int> co) override;
 };
 
 #endif
