ef6781b6e84d57c7fbe23cdd4d00c59992cc9dd0
Allen <allenfan@live.com>
2016-07-14 20:41:37 -0400

    controller update!

--- code/Controller.cc
+++ code/Controller.cc
@@ -16,7 +16,18 @@ int main(){
 		if (input == "setup"){
 			curGame->setup();
 		} else if (input == "game"){
-			runGame();
+			Player p[numPlayers];
+			for (int i = 0 ; i < numPlayers; ++i){
+				string arg;
+				cin >> arg;
+				if (arg == "human"){
+					p[i] = Human(); 
+				} else {
+					int lvl = arg[9] - '0';
+					p[i] = Ai(lvl);
+				}
+			}
+			runGame(p);
 		} else {
 			cerr << "Invalid Command! Please enter setup or game." << endl;
 		}
@@ -24,7 +35,23 @@ int main(){
 }
 
 
-void runGame(){
-	int moveCount = 0;
-	
+void runGame(Player p[]){
+	int start = curGame.getStartPlayer();
+	for (int moveCount = 0, bool result =true ; result ; ++moveCount){
+		Move m = player[(moveCount + start)%numPlayers].getMove();
+		int moveResult = curGame.executeMove(m);
+		result = reactToState(moveResult,(moveCount + start)%numPlayers);
+	}
 }
+
+bool reactToState(int i, int curPlayer){
+	if (i == 1){
+		cout << "Checkmate! " << (curPlayer ? "Black" : "White") << " wins!" << endl;
+	}
+
+}
+
+
+
+
+
