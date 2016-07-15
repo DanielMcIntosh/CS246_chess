32b7d839e8eb5af3b856b0bd9d94e641c3031527
Allen <allenfan@live.com>
2016-07-14 20:10:34 -0400

    UML + controller update

--- code/Controller.cc
+++ code/Controller.cc
@@ -4,12 +4,27 @@
 #include <iostream>
 #include <vector>
 #include <utility>
+#define numPlayers 2
 
-
+using namespace std;
 
 int main(){
 	Game * curGame = new Game{}; // need edit
+	int score [numPlayers] = {0};
 	string input;
-	while (cin >> input)
+	while (cin>>input){
+		if (input == "setup"){
+			curGame->setup();
+		} else if (input == "game"){
+			runGame();
+		} else {
+			cerr << "Invalid Command! Please enter setup or game." << endl;
+		}
+	}
+}
 
+
+void runGame(){
+	int moveCount = 0;
+	
 }
