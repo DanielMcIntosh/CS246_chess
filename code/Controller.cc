b5cdb02aba78ef4c616cbd7ea96b337e2c5fe6a3
Allen <allenfan@live.com>
2016-07-14 21:03:00 -0400

    controller.cc first finish

--- code/Controller.cc
+++ code/Controller.cc
@@ -32,6 +32,9 @@ int main(){
 			cerr << "Invalid Command! Please enter setup or game." << endl;
 		}
 	}
+	cout << "Final Score:" << endl;
+	cout << "White: " << score[0] << endl;
+	cout << "Black: " << score[1] << endl;
 }
 
 
@@ -40,16 +43,34 @@ void runGame(Player p[]){
 	for (int moveCount = 0, bool result =true ; result ; ++moveCount){
 		Move m = player[(moveCount + start)%numPlayers].getMove();
 		int moveResult = curGame.executeMove(m);
+		if (!moveResult){
+			--moveCount;
+			cerr << "Invalid Move" << endl;
+			continue;
+		}
 		result = reactToState(moveResult,(moveCount + start)%numPlayers);
 	}
 }
 
 bool reactToState(int i, int curPlayer){
-	if (i == 1){
-		cout << "Checkmate! " << (curPlayer ? "Black" : "White") << " wins!" << endl;
+	if (i == 3){
+		cout << "Checkmate! ";
 	}
-
+	if (i >= 2){
+		cout << (((bool)curPlayer == (bool)(i%2)) ? "Black" : "White") << " wins!" << endl;
+		return false;
+	} else if (i == 1){
+		cout << "Stalemate!" << endl;
+		return false;
+	} else if (i == -1){
+		return true;
+	} else {
+		cout << (curPlayer ? "White" : "Black") << " is in check." << endl;
+		return true;
+	}
 }
+
+
 
 
 
