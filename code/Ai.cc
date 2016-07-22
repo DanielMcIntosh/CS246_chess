117ebf9d0b0a24f865bc2e89c61ae2b056e36056
Daniel McIntosh <Daniel@DanielMcIntosh.ca>
2016-07-22 15:55:09 -0400

    finished AI?

--- code/Ai.cc
+++ code/Ai.cc
@@ -18,7 +18,7 @@ Move Ai::getMove(Game *g)
 		{
 			for (int y = 0; y < 8; ++y)
 			{
-				Move curMove = new Move(*i, make_pair(x, y), colour ? 'q' : 'Q');
+				Move curMove(*i, make_pair(x, y), colour ? 'q' : 'Q');
 				int curPriority = Game->tryMove(curMove, priorityMasks[this->lvl]);
 				if (curPriority > maxPriority)
 				{
@@ -33,7 +33,18 @@ Move Ai::getMove(Game *g)
 		}
 	}
 
-	return Move(make_pair(0, 0), make_pair(0, 0), 0);
+	if (maxPriority <= 0)
+	{
+		cerr << "error, called getMove() after stalemate!";
+		return Move(make_pair(0, 0), make_pair(0, 0), 0);
+	}
+
+	unsigned seed = chrono::system_clock::now().time_since_epoch().count();
+	default_random_engine generator (seed);
+
+	uniform_int_distribution<int> distribution(0, topMoves.size()-1);
+
+	return topMoves[distribution(generator)];
 }
 
 Ai::Ai(int lvl, bool colour, vector< pair<int, int> > myPieces): lvl{lvl}, colour{colour}, myPieces{myPieces}
