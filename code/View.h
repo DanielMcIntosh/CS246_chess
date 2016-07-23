6910fa605e0f8460922e512d7b2b958e58fe29eb
vusause <mikevu123@gmail.com>
2016-07-23 17:07:29 -0400

    Fixed View

--- code/View.h
+++ code/View.h
@@ -6,7 +6,9 @@ class View {
 public:
     View(std::istream &in, std::ostream &out);
     friend std::istream& operator>>(std::istream &is);
+    friend std::ostream& operator<<(std::ostream &os);
     void reactToTurn(int result, int player);
     void printScore(int wScore, int bScore);
     void boardCheck(bool valid);
+    void welcome();
 }
