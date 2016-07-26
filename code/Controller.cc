bd8eeb127a6d34edf37255ff6c687316efad32c6
Allen <allenfan@live.com>
2016-07-25 22:25:34 -0400

    controller update

--- code/Controller.cc
+++ code/Controller.cc
@@ -54,7 +54,13 @@ int main(){
 				}
 			}
 
-			runGame(p, curGame);
+			int winner = runGame(p, curGame);
+			if (winner == -1){
+				score[0] += 0.5;
+				score[1] += 0.5;
+			} else {
+				++score[winner];
+			}
 
 			for (int i = 0; i < numPlayers; ++i)
 			{
@@ -86,12 +92,12 @@ int main(){
 }
 
 
-void runGame(Player *p[], Game *curGame){
+int runGame(Player *p[], Game *curGame){
 	cout << *curGame;
 
 	int start = curGame->getStartPlayer();
-	bool result = true;
-	for (int moveCount = 0; result ; ++moveCount){
+	int result = 0;
+	for (int moveCount = 0; !result ; ++moveCount){
 		Move m = p[(moveCount + start)%numPlayers]->getMove(curGame);
 		int moveResult = curGame->executeMove(m);
 		if (!moveResult){
@@ -104,10 +110,13 @@ void runGame(Player *p[], Game *curGame){
 		result = reactToState(moveResult,(moveCount + start)%numPlayers);
 		cout << *curGame;
 	}
+	if (result < 0)
+		return -1;
+	return result % 2;
 }
 
 //returns false when endgame state reached
-bool reactToState(int state, int curPlayer){
+int reactToState(int state, int curPlayer){
 	if (state == 3){ //check mate
 		cout << "Checkmate! ";
 	}
@@ -115,9 +124,10 @@ bool reactToState(int state, int curPlayer){
 		cout << (((bool)curPlayer == (bool)(state%2)) ? "Black" : "White") << " wins!" << endl;
 	} else if (state == 1){ //stalemate
 		cout << "Stalemate!" << endl;
+		return -1;
 	} else if (state == -1){} //normal
 	else if (state == -2){ //check
 		cout << (curPlayer ? "White" : "Black") << " is in check." << endl;
-	}
-	return (state < 0);
+	} 
+	return (state > 0) ? ((curPlayer + state) % 2 + 1) : 0;
 }
